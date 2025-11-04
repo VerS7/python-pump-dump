4035009cbe853c419ead743ee34252661742e52b
VerS7 <thegoversus@gmail.com>
2025-11-04 08:25:43 +0300

    more status info

--- status.sh
+++ status.sh
@@ -3,9 +3,8 @@
 PID_FILE="main.pid"
 
 if [ -f "$PID_FILE" ] && kill -0 $(cat $PID_FILE) 2>/dev/null; then
-    echo "Процесс работает с PID: $(cat $PID_FILE)"
-    echo "Использует ресурсов:"
-    ps -p $(cat $PID_FILE) -o %mem,%cpu 
+    echo -e "Процесс работает с PID: $(cat $PID_FILE)\n"
+    ps -p $(cat $PID_FILE) -o lstart,etime,%mem,%cpu
 else
     echo "Процесс не запущен"
 fi
